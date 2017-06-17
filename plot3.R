83a9e622642d53c5a10c41ca80b3e4c0910b8600
Pedro I. Rivera-Vega <p.rivera@upr.edu>
2017-06-17 14:27:19 -0400

    After correcting colors in plot3 and in plot4.

--- plot3.R
+++ plot3.R
@@ -13,7 +13,7 @@ points(hpc3$DateTime, hpc3$Sub_metering_1, type="l")
 points(hpc3$DateTime, hpc3$Sub_metering_2, type="l", col="red")
 points(hpc3$DateTime, hpc3$Sub_metering_3, type="l", col="blue")
 legend("topright", lty=1, lwd=2, 
-       col=c("black", "blue", "red"), 
+       col=c("black", "red", "blue"), 
        legend = c("Sub_metering_1", 
                   "Sub_metering_2", 
                   "Sub_metering_3"))
